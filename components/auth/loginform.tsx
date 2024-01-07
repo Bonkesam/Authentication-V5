57edbea2b5fa5a4f180e7d8b03c9c363c2f2b7b2
Bonkesam <bonkesam9@gmail.com>
2024-01-07 12:21:00 +0300

    Adds the success and error messages

--- components/auth/loginform.tsx
+++ components/auth/loginform.tsx
@@ -18,6 +18,8 @@ import {
 
 import CardWrapper from './card-wrapper'
 import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
 
 const LoginForm = () => {
 
@@ -27,7 +29,11 @@ const LoginForm = () => {
       email: "",
       password: "",
     }
-  })
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  }
   return (
     <CardWrapper
         headerLabel="Welcome back"
@@ -36,7 +42,7 @@ const LoginForm = () => {
         showSocial
     >
       <Form {...form}>
-       <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
             <FormField
               control={form.control}
@@ -74,6 +80,9 @@ const LoginForm = () => {
             />
             
           </div>
+          <FormError message=""/>
+
+          <FormSuccess  message=""/>
           <Button type="submit" className="w-full">
             Login
           </Button> 
