77d517853e6b1cdf4f32af5bf6b8c94872a69b7d
Bonkesam <bonkesam9@gmail.com>
2024-01-10 22:35:50 +0300

    Adds the github client id and secret

--- auth.config.ts
+++ auth.config.ts
@@ -12,7 +12,10 @@ import Google from "next-auth/providers/google";
 export default {
   providers: [
   Google,
-  Github,
+  Github({
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
