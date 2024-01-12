43b148d0a0e41cc8fafdf0a721ecee79983d89d4
Bonkesam <bonkesam9@gmail.com>
2024-01-12 17:20:50 +0300

    creates new email verification server action

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, { useCallback, useEffect } from 'react'
+import CardWrapper from '@/components/auth/card-wrapper';
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from 'next/navigation';
+
+
+const newVerificationForm = () => {
+
+    const searchParams = useSearchParams();
+
+    const token = searchParams.get("token");
+
+    const onSubmit = () => {
+        useCallback(() => {
+           console.log(token); 
+        }, [token]);
+
+        useEffect(() => {
+           onSubmit(); 
+        }, [onSubmit])
+    }
+  return (
+    <CardWrapper 
+    headerLabel='Confirming your verification'
+    backButtonLabel='Back to login'
+    backButtonHref='/auth/login'
+    >
+        <div className='flex items-center w-full justify-center'>
+            <BeatLoader />
+        </div>
+    </CardWrapper>
+  )
+}
+
+export default newVerificationForm;
