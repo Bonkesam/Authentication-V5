468e08b731f130e3593db3517fc8ba2c984aa503
Bonkesam <bonkesam9@gmail.com>
2024-01-15 23:39:28 +0300

    Creates the two factor authentication util for fetchong emails

--- data/two-factor-token.ts
+++ data/two-factor-token.ts
@@ -11,3 +11,15 @@ export const getTwoFactorTokenByToken =  async (token: string) => {
         return null;
     }
 }
+
+export const getTwoFactorTokenByEmail =  async (email: string) => {
+    try {
+        const twoFactorToken = await db.twoFactorToken.findFirst({
+            where: { email }
+        });
+
+        return twoFactorToken;
+    } catch (error) {
+        return null;
+    }
+}
