5fcd09d0e4cf62113fc8fe89c7b09e8dc147a0f0
Bonkesam <bonkesam9@gmail.com>
2024-02-07 13:05:24 +0300

    Adds the new signout in the settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,21 +1,23 @@
 'use client';
 
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 
 const SettingsPage = () => {
 
   const session = useSession();
+
+  const onClick = () => {
+    signOut();
+  };
   
   return (
     <div>
-      {JSON.stringify(session)}
+      {JSON.stringify(session)} 
 
-      <form>
-        <button type="submit">
-          Sign Out
-        </button>
-      </form>
+      <button onClick={onClick} type="submit">
+        Sign Out
+      </button>    
       
     </div>
   )
