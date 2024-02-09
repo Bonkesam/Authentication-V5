ee8ee70031d4fd05ba828e7d8b2cb31ba628518d
Bonkesam <bonkesam9@gmail.com>
2024-02-09 18:55:53 +0300

    Adds the rolegate component

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import { useCurrentRole } from '@/hooks/use-current-role';
+import RoleGate from '@/components/auth/RoleGate';
+import { FormSuccess } from '@/components/form-success';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { UserRole } from '@prisma/client';
 import React from 'react'
 
 const AdminPage = () => {
 
-    const role = useCurrentRole();
   return (
-    <div>
-      Current Role: {role}
-    </div>
+    <Card className='w-[600px]'>
+        <CardHeader>
+            <p className='text-2xl font-semibold text-center'>
+               Admin 
+            </p>
+        </CardHeader>
+        <CardContent className='space-y-4'>
+           <RoleGate allowedRole={UserRole.ADMIN}>
+                <FormSuccess message='You are allowed to see this content'/>
+            </RoleGate> 
+        </CardContent>
+    </Card>
   )
 }
 
