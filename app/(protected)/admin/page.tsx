ab87b5f986e08142641a4c029647f152ed1594f4
Bonkesam <bonkesam9@gmail.com>
2024-02-09 19:53:38 +0300

    Adds the Server api route

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -2,11 +2,24 @@
 
 import RoleGate from '@/components/auth/RoleGate';
 import { FormSuccess } from '@/components/form-success';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { UserRole } from '@prisma/client';
 import React from 'react'
+import { toast } from 'sonner';
 
 const AdminPage = () => {
+
+    const onApiRouteClick = () => {
+        fetch("/api/admin")
+        .then((response) => {
+            if (response.ok) {
+                toast.success("Allowed API route")
+            } else {
+                toast.error("Forbidden API route")
+            }
+        })
+    }
 
   return (
     <Card className='w-[600px]'>
@@ -19,6 +32,26 @@ const AdminPage = () => {
            <RoleGate allowedRole={UserRole.ADMIN}>
                 <FormSuccess message='You are allowed to see this content'/>
             </RoleGate> 
+
+            <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
+                <p className='text-sm font-medium'>
+                    Admin-only API route
+                </p>
+                <Button>
+                     Click to test
+                </Button>
+    
+            </div>
+
+            <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
+                <p className='text-sm font-medium'>
+                    Admin-only Server Action
+                </p>
+                <Button>
+                     Click to test
+                </Button>
+    
+            </div>
         </CardContent>
     </Card>
   )
