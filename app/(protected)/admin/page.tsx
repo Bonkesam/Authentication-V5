79a86bf1eb638b9e798d2a0f4183cf4bbb6776b9
Bonkesam <bonkesam9@gmail.com>
2024-02-09 18:36:27 +0300

    Adds theget current role hook

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useCurrentRole } from '@/hooks/use-current-role';
 import React from 'react'
 
 const AdminPage = () => {
+
+    const role = useCurrentRole();
   return (
     <div>
-      
+      Current Role: {role}
     </div>
   )
 }
